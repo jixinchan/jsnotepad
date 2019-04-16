8978fdb2991b709da72b9877891bf397a614f791
jixinchan <1209980074@qq.com>
2019-04-16 20:23:51 +0800

    完善字体对话框

--- dlg-font/dlg-font.js
+++ dlg-font/dlg-font.js
@@ -11,17 +11,17 @@ var $dlgFont = (function(){
     +'      <div class="box">'
     +'      <div class="box1">'
     +'        <label>字体(F):</label>'
-    +'        <input type="text" value="楷体"/>'
+    +'        <input type="text" value="楷体" class="familyinp"/>'
     +'        <div class="family"></div>'
     +'      </div>'
     +'      <div class="box2">'
     +'        <label>字形(Y):</label>'
-    +'        <input type="text" value="常规"/>'
+    +'        <input type="text" value="常规" class="styleinp"/>'
     +'        <div class="style"></div>'
     +'      </div>'
     +'      <div class="box3">'
     +'        <label>大小(S):</label>'
-    +'        <input type="text" value="小四"/>'
+    +'        <input type="text" value="小四" class="sizeinp"/>'
     +'        <div class="size"></div>'
     +'      </div>'
     +'      </div>'
@@ -44,11 +44,51 @@ var $dlgFont = (function(){
   var $dlg = $(DOM),
       $btnConfirm = $dlg.find('.btn-confirm'),
       $btnCancel = $dlg.find('.btn-cancel'),
-      $btnClose = $dlg.find('.btn-close');
+      $btnClose = $dlg.find('.btn-close'),
+      $familyBox=$dlg.find('.family'),
+      $styleBox=$dlg.find('.style'),
+      $sizeBox=$dlg.find('.size'),
+      $familyInp=$dlg.find('.familyinp'),
+      $styleInp=$dlg.find('.styleinp'),
+      $sizeInp=$dlg.find('.sizeInp');
 
+  function initBox(ele,list,f){
+    var $List='<ul class="list">';
+    if(f=='F'){
+      for(var j=0;j<list.length;j++){
+        $List+='<li style="font-family:'+list[j]+'">'+list[j]+'</li>';
+      }
+    }else if(f=='Y'){
+      for(var i=0;i<list.length;i++){
+        if(list[i]==='斜体'){
+          $List+='<li style="font-style:italic">'+list[i]+'</li>';
+        }
+        if(list[i]==='粗体'){
+          $List+='<li style="font-weight:bold">'+list[i]+'</li>';
+        }
+        if(list[i]==='粗偏斜体'){
+          $List+='<li style="font-style:italic;font-weight:bold">'+list[i]+'</li>';
+        }
+
+      }
+    }else{
+      for(var m=0;m<list.length;m++){
+        $List+='<li>'+list[m]+'</li>';
+      }
+    }
+    $List+='</ul>';
+    return $List;
+  }
+  
   function show(){
+    var familys=['Agency FB', 'Algerian', 'Arial', 'Arial Rounded MT', 'Axure Handwriting', 'Bahnschrift', 'Baskerville Old Face', 'Bauhaus 93', 'Bell MT', 'Berlin Sans FB', 'Bernard MT', 'BlackAdder ITC'];
+    var styles=['常规', '斜体', '粗体', '粗偏斜体'];
+    var sizes=['8', '9', '10', '11', '12', '14', '16', '18', '20', '22', '24', '26', '28', '36', '48', '72'];
     $('body').append($dlg);
-   
+    $familyBox.append(initBox($familyInp,familys,'F'));
+    $styleBox.append(initBox($styleInp,styles,'Y'));
+    $sizeBox.append(initBox($sizeInp,sizes),'S');
+
     $dlg.find('.dialogbox').draggable({handle:$dlg.find('.titlebar')});
     $btnCancel.click(function(){$dlg.remove();});
     $btnConfirm.click(function(){$dlg.remove();});
